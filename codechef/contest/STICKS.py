d4155563cdb4801b98067ef0aa5febfade205ff2
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-09-08 01:16:24 +0100

    Omo, counting and storing ni problem yi sha, can you imagine?

--- codechef/contest/STICKS.py
+++ codechef/contest/STICKS.py
@@ -1,34 +1,17 @@
-def answer(array):
-    print(array)
-    n = len(array)
-    H = max(array) - 1
-    i, count = 0, 0
-
-    if all(values == array[0] for values in array):
-        return 1
-
-    while i < n:
-        if array[i] - H <= 0:
-            array.pop(i)
-            n -= 1
-        else:
-            array[i] = array[i] - H
-            i += 1
-        count += 1
-    if all(values == array[0] for values in array):
-        return count
-    return 0
-
-    # return array
-
 if __name__ == '__main__':
     try:
-        values = []
         T = int(input())
         for _ in range(T):
             N = int(input())
-            values.append(list(map(int, input().split())))
-        for value in values:
-            print(answer(value))
+            values = list(map(int, input().split()))
+            store = {}
+            for value in values:
+                if value not in store:
+                    store[value] = 0
+                store[value] += 1
+            if 0 in values:
+                print(store, len(store)-1)
+            else:
+                print(store, len(store))
     except Exception:
         pass
